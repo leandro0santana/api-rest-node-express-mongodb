7ef9a693968b8fac74210395ca08c047902d6752
Leandro <leandro.c.santana@hotmail.com>
2020-05-23 01:37:43 -0300

    Criação de token para usuário troca a senha com tempo de expiração, adição de duas nova colunas no database/user.js para o token e o tempo de expiração

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -4,9 +4,8 @@ const authConfig = require('../../config/auth');
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if(!authHeader) {
+  if(!authHeader) 
     return res.status(401).send({ error: 'No token provided'});
-  }
 
   const parts = authHeader.split(' ');
 
